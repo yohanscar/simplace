c50edcad1bbc54b352f44d55f02493a10ceeca56
Yohan <wyohan@gmail.com>
2017-04-20 18:47:09 -0300

    ajustes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var router = express.Router();
 
+router.use(bodyParser.json()); // for parsing application/json
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Simplaces | As melhores oficinas' });
 });
+
+/*POST */
+router.post('/', function (req, res) {
+
+    try {
+        res.send(req.body)
+    } catch (error) {
+        res.status(500).send(error)
+    }
+ })
 
 module.exports = router;
