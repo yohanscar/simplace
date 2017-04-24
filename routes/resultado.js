07e544de59a4135a3edc6e76a282497766f0a51a
Yohan <wyohan@gmail.com>
2017-04-23 22:14:22 -0300

    ajuste

--- routes/resultado.js
+++ routes/resultado.js
@@ -13,19 +13,22 @@ var G_endereco;
 var sOficinas = "Oficinas em ";
 var aux_endereco;
 var coordenadas = "[{position: new google.maps.LatLng(-33.91721, 151.22630),type: 'info'      }, {      position: new google.maps.LatLng(-33.91539, 151.22820),      type: 'info'      }, {      position: new google.maps.LatLng(-33.91747, 151.22912),      type: 'info'      }, {      position: new google.maps.LatLng(-33.91910, 151.22907),      type: 'info'      }, {      position: new google.maps.LatLng(-33.91725, 151.23011),      type: 'info'      }, {      position: new google.maps.LatLng(-33.91872, 151.23089),      type: 'info'      }, {      position: new google.maps.LatLng(-33.91784, 151.23094),      type: 'info'     }, {      position: new google.maps.LatLng(-33.91682, 151.23149),      type: 'info'      }, {      position: new google.maps.LatLng(-33.91790, 151.23463),      type: 'info'      }, {      position: new google.maps.LatLng(-33.91666, 151.23468),      type: 'info'      }, {      position: new google.maps.LatLng(-33.916988, 151.233640),      type: 'info'      }, {      position: new google.maps.LatLng(-33.91662347903106, 151.22879464019775),      type: 'parking'      }, {      position: new google.maps.LatLng(-33.916365282092855, 151.22937399734496),      type: 'parking'      }, {      position: new google.maps.LatLng(-33.91665018901448, 151.2282474695587),      type: 'parking'      }, {      position: new google.maps.LatLng(-33.919543720969806, 151.23112279762267),      type: 'parking'      }, {      position: new google.maps.LatLng(-33.91608037421864, 151.23288232673644),      type: 'parking'      }, {      position: new google.maps.LatLng(-33.91851096391805, 151.2344058214569),      type: 'parking'      }, {      position: new google.maps.LatLng(-33.91818154739766, 151.2346203981781),      type: 'parking'      }, {      position: new google.maps.LatLng(-33.91727341958453, 151.23348314155578),      type: 'library'      }      ];"
-
+var _rating;
 
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  //res.send(req.query.cep);
+  //res.send(req.query.cep); 
   _cep = req.query.cep;
+  _rating = req.query.rating;
 
 GooglePlacesClient.geocode({
   address: _cep
 }, function(err, response) {
   if (!err) {
-             console.log(response.json.results);  // mostra valores do cep
+             console.log("rating entrada>>>", _rating)
+             console.log(response.json.results[0].address_components);
+             console.log(response.json.results[0].geometry);  // mostra valores do cep
              G_endereco = response.json.results[0].formatted_address;   
     	     GooglePlacesClient.places({
                    query: sOficinas + G_endereco // oficinas em <endereco google>
@@ -39,11 +42,9 @@ GooglePlacesClient.geocode({
                         places.details({placeid: aux_place_id}, function(err, response) {
                             if(err) { console.log(err); return; }
                              console.log("search details: ", response.result.geometry.location);
-
-
-
-
-                             res.render('resultado', { title: 'Simplaces | As melhores oficinas' , codigo: _cep , coords: coordenadas});
+                               res.send("<h1>teste</h1> <h2>teste</h2>");
+                            // res.send(response.result.geometry.location);
+                            // res.render('resultado', { title: 'Simplaces | As melhores oficinas' , codigo: _cep  });
                              });
                 }
              })
